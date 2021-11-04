756df7ce09b8e209716d810bfe4ccd9353b38c64
ageng subagja <agengsubagja001@gmail.com>
2021-11-04 11:08:20 +0700

    first commit

---
+++ application/views/dashboard.php
@@ -0,0 +1,41 @@
+<!-- head -->
+<?php $this->load->view('partial/head'); ?>
+
+<?php $this->load->view('partial/navbar'); ?>
+
+<section>
+    <!-- <div class="container">
+        <div class="col col-md-12 text-center mt-3">
+            <span class="" style="font-size:20px">Bicara Wanita</span>
+        </div>
+        <div class="mt-5">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" style="">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2021/10/29/798f1d1e-8208-4ce2-9af4-8301e893a14b.jpg.webp?ect=3g" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2021/10/29/7669d78d-14c5-46b0-af5d-baa6929482dc.jpg.webp?ect=3g" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2021/7/30/74d32a7f-6a2d-49a3-b325-114de4b055c5.jpg.webp?ect=3g" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div> -->
+   
+</section>
