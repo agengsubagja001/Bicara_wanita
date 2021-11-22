9074f8b4ccef153b52d48b68adc491f42a310437
faishol17 <faishol12permana@gmail.com>
2021-11-22 16:11:12 +0700

    update navbar dan head

--- application/views/pembaca/partial/head.php
+++ application/views/pembaca/partial/head.php
@@ -18,6 +18,8 @@
 
      <!-- CDN CKEDITOR -->
      <script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
+     <!-- favicon -->
+    <link rel="icon" href="<?php echo base_url('favicon.ico'); ?>" sizes="16x16" />
 <style>
     /* css text destok */
     @media only screen and (min-width: 992px) {
